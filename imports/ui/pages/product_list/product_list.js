316ebf7e2b13d1db8e2527690fd1f0b611b1b902
lucky <jaiswal.lucky8888@gmail.com>
2019-09-01 22:55:12 +1000

    # Added pagination UI to product page

--- imports/ui/pages/product_list/product_list.js
+++ imports/ui/pages/product_list/product_list.js
@@ -4,3 +4,4 @@ import './product_list.html';
 import '../../components/product_page_navigation/product_page_navigation.js';
 import '../../components/branch_dropdown/branch_dropdown.js';
 import '../../components/product_display/product_display.js';
+import '../../components/pagination/pagination.js';
