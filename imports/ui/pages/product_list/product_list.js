3b450883e258f5b0826edc2655957daea00a9efe
lucky <jaiswal.lucky8888@gmail.com>
2019-09-01 20:41:34 +1000

    # Update and structure product page into proper meteor project file structure

--- imports/ui/pages/product_list/product_list.js
+++ imports/ui/pages/product_list/product_list.js
@@ -1,1 +1,6 @@
 import './product_list.html';
+
+//
+import '../../components/product_page_navigation/product_page_navigation.js';
+import '../../components/branch_dropdown/branch_dropdown.js';
+import '../../components/product_display/product_display.js';
