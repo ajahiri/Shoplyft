de6afdb3b029387c233df7bd49413ac74bb273db
Arian Jahiri <ajahiri.ait@gmail.com>
2019-09-08 12:36:42 +1000

    Graphics and UI Fixes for Android
    - Fixes for deploying to Android APK

--- imports/ui/components/navigation/navigation.js
+++ imports/ui/components/navigation/navigation.js
@@ -2,8 +2,15 @@ import './navigation.html';
 
 Template.navigation.onRendered(function() {
   //Below line is a fix for bug where the shadow-overlay remains when nav to different page. -Arian
+  /*$('.sidenav-overlay').css({"display": "none","opacity": "0"});
+  $('.sidenav').sidenav(elems, {
+    preventScrolling: false,
+  });*/
   $('.sidenav-overlay').css({"display": "none","opacity": "0"});
-  $('.sidenav').sidenav();
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, {
+    preventScrolling: false,
+  });
 });
 
 Template.navigation.events({
---
+++ mobile-config.js
@@ -0,0 +1,38 @@
+// This section sets up some basic app metadata, the entire section is optional.
+App.info({
+  id: 'com.annulus_studios.shoplyft',
+  name: 'ShopLyft',
+  description: 'Making shopping easy.',
+  author: 'Annulus Studios',
+  email: 'annulus.studios@gmail.com',
+  website: 'https://shoplyft.me/',
+  version: '0.1.0',
+  buildNumber: '1'
+});
+// Set up resources such as icons and launch screens.
+App.icons({
+  'android_mdpi': 'androidAssets/icons/icon-48-mdpi.png',
+  'android_hdpi': 'androidAssets/icons/icon-72-hdpi.png',
+  'android_xhdpi': 'androidAssets/icons/icon-96-xhdpi.png',
+  'android_xxhdpi': 'androidAssets/icons/icon-144-xxhdpi.png',
+  'android_xxxhdpi': 'androidAssets/icons/icon-192-xxxhdpi.png',
+  // More screen sizes and platforms...
+});
+App.launchScreens({
+  'android_ldpi_portrait': 'androidAssets/splashes/splash-port-ldpi.png',
+  'android_mdpi_portrait': 'androidAssets/splashes/splash-port-mdpi.png',
+  'android_hdpi_portrait': 'androidAssets/splashes/splash-port-hdpi.png',
+  'android_xhdpi_portrait': 'androidAssets/splashes/splash-port-xhdpi.png',
+  // More screen sizes and platforms...
+});
+
+// Set PhoneGap/Cordova preferences.
+App.setPreference('HideKeyboardFormAccessoryBar', true);
+App.setPreference('Orientation', 'default');
+
+//Push Notifications
+/*App.configurePlugin('phonegap-plugin-push', {
+  SENDER_ID: 659123908175
+});*/
+
+//App.accessRule("*");
