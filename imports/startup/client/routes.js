d40e2b90d668356e00c0d026f4470d65bfda6c13
Nathan13786809 <nathan.ibarcena@gmail.com>
2019-09-11 16:30:15 +1000

    merging with master

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -16,31 +16,11 @@ import '../../ui/pages/signup/signup.js';
 
 // Admin Homepage
 import '../../ui/pages/admin-home/admin-home.js';
-<<<<<<< HEAD
-// View Branches
-import '../../ui/pages/admin-home/view-branches/view-branches.js';
-// Add/Delete Products
-import '../../ui/pages/admin-home/Add-Del-Products/Add-Del-Products.js';
-// View Customers
-import '../../ui/pages/admin-home/view-customers/view-customers.js';
-// Add Sellers
-import '../../ui/pages/admin-home/add-seller/add-seller.js';
-
-// Seller Homepage
-import '../../ui/pages/seller-home/seller-home.js';
-// Transaction History
-import '../../ui/pages/seller-home/view-transactions/view-transaction.js';
-// Update Stock
-import '../../ui/pages/seller-home/update-stock/update-stock.js';
-// Add/Delete Products
-import '../../ui/pages/seller-home/Add-Del-Products/Add-Del-Products.js';
-=======
 //All admin functions should be imported in the admin-home.js
 
 // Seller Homepage
 import '../../ui/pages/seller-home/seller-home.js';
 //All seller functions should be imported in the seller-home.js
->>>>>>> master
 
 
 // Set up all routes in the app
@@ -73,72 +53,10 @@ FlowRouter.route('/admin-home', {
   },
 });
 
-<<<<<<< HEAD
-FlowRouter.route('/admin-home/view-branches', {
-  name: 'App.admin-home.view-branches',
-  action() {
-    BlazeLayout.render('App_body', { main: 'App_adminHome_view-branches' });
-  },
-});
-
-FlowRouter.route('/admin-home/Add-Del-Products', {
-  name: 'App.admin-home.Add-Del-Products',
-  action() {
-    BlazeLayout.render('App_body', { main: 'App_adminHome_Add-Del-Products' });
-  },
-});
-
-FlowRouter.route('/admin-home/view-customers', {
-  name: 'App.admin-home.view-customers',
-  action() {
-    BlazeLayout.render('App_body', { main: 'viewCustomers' });
-  },
-});
-
-FlowRouter.route('/admin-home/add-seller', {
-  name: 'App.admin-home.add-seller',
-  action() {
-    BlazeLayout.render('App_body', { main: 'addSeller' });
-  },
-});
-
-
-=======
 //Respective routes for seller pages are in seller-home js
->>>>>>> master
 FlowRouter.route('/seller-home', {
   name: 'App.seller-home',
   action() {
     BlazeLayout.render('App_body', { main: 'App_sellerHome' });
   },
 });
-<<<<<<< HEAD
-
-FlowRouter.route('/seller-home/view-transactions', {
-  name: 'App.seller-home.view-transactions',
-  action() {
-    BlazeLayout.render('App_body', { main: 'App_sellerHome_view-transactions' });
-  },
-});
-
-FlowRouter.route('/seller-home/update-stock', {
-  name: 'App.seller-home.update-stock',
-  action() {
-    BlazeLayout.render('App_body', { main: 'App_sellerHome_update-stock' });
-  },
-});
-
-FlowRouter.route('/seller-home/Add-Del-Products', {
-  name: 'App.seller-home.Add-Del-Products',
-  action() {
-    BlazeLayout.render('App_body', { main: 'App_sellerHome_Add-Del-Products' });
-  },
-});
-
-FlowRouter.notFound = {
-  action() {
-    BlazeLayout.render('App_body', { main: 'App_notFound' });
-  },
-};
-=======
->>>>>>> master
