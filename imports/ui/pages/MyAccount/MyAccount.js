cedb4c2f37ebbb8e14ad329323d789be31b36e07
Nathan13768609 <nathan.ibarcena@gmail.com>
2019-09-24 21:16:29 +1000

    MyAccount 95% complete

--- imports/ui/pages/MyAccount/MyAccount.js
+++ imports/ui/pages/MyAccount/MyAccount.js
@@ -1,1 +1,27 @@
 import './MyAccount.html';
+import { Meteor } from 'meteor/meteor';
+import '../../components/userList/userList.js';
+
+Template.App_MyAccount.helpers({
+    getUsername() {
+        return Meteor.user().username;
+    }
+});
+
+Template.App_MyAccount.helpers({
+    getEmail() {
+        return Meteor.user().emails[0].address;
+    }
+});
+
+Template.App_MyAccount.helpers({
+    verified() {
+        return Meteor.user().emails[0].verified;
+    }
+});
+
+Template.App_MyAccount.helpers({
+    getId() {
+        return Meteor.user()._id;
+    }
+});
