1b0ff1f44df97a655fd681816b9f354519d3820c
lucky <jaiswal.lucky8888@gmail.com>
2019-09-30 00:02:32 +1000

    Added Buy Now button on checkout page.
    This button redirects to payment confirm page on click.

--- imports/ui/pages/checkout/checkout.js
+++ imports/ui/pages/checkout/checkout.js
@@ -2,3 +2,12 @@ import './checkout.html';
 
 import '../../components/product_page_navigation/product_page_navigation.js';
 import '../../components/branch_dropdown/branch_dropdown.js';
+
+if (Meteor.isClient) {
+  Template.checkout.events({
+    'click #buy_now': function(e){
+     console.log("You pressed the button");
+     FlowRouter.go('payment_confirm');
+   }
+});
+}
