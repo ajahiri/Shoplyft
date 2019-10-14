22001597381b222bb5c593da17b98bc806d2ff99
Nathan13768609 <nathan.ibarcena@gmail.com>
2019-10-14 21:27:43 +1100

    Recommendations (not working yet)

--- imports/ui/pages/orderSuccess/orderSuccess.js
+++ imports/ui/pages/orderSuccess/orderSuccess.js
@@ -1,1 +1,21 @@
 import './orderSuccess.html';
+import { Products } from '../../../api/branches/branches';
+import { Orders } from '../../../api/orders/orders.js';
+import { Meteor } from 'meteor/meteor';
+
+Template.orderSuccess.onRendered(function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
+});
+
+Template.recommendedProducts.helpers({
+    recommendedProducts() {
+        return Products.find({category: Orders.findOne({owner: Meteor.userId()}, { sort: { createdAt: -1 } }).category});
+    },
+    recommendationId() {
+        return Products.find({category: Orders.findOne({owner: Meteor.userId()}, { sort: { createdAt: -1 } })._id});
+    },
+    imageURL() {
+        return Products.find({category: Orders.findOne({owner: Meteor.userId()}, { sort: { createdAt: -1 } }).imageURL});
+    }
+});
